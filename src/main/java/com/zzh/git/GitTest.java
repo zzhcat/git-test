fca8b38e8fd51b28a0f46f51d6f886de10f3262b
zzh <3081309881@qq.com>
2022-03-18 20:45:20 +0800

    commit

--- src/main/java/com/zzh/git/GitTest.java
+++ src/main/java/com/zzh/git/GitTest.java
@@ -7,5 +7,6 @@ package com.zzh.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
