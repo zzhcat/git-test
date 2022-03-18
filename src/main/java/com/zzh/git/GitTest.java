7dae481d9c35dc0a5412afa6ce4c4e45b89c2c15
zzh <3081309881@qq.com>
2022-03-18 20:31:01 +0800

    commit

---
+++ src/main/java/com/zzh/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.zzh.git;
+
+/**
+ * @author 赵志海
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
