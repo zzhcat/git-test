5dcd8f4a149523f8b41f035f72d26864667cf797
zzh <3081309881@qq.com>
2022-03-19 18:00:58 +0800

    master-commit2

--- src/main/java/com/zzh/git/GitTest.java
+++ src/main/java/com/zzh/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello master test");
     }
 }
